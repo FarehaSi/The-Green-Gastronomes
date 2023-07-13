0dfbd7cfdbc8507fa5461c18fe414ec5fe349268
FarehaSi <>
2023-07-13 14:36:45 +0000

    Create comments model

--- blog/models.py
+++ blog/models.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from django.contrib.postgres.fields import ArrayField
 
-STATUS = ((0, "Draft"), (1, "Published"))
+STATUS = ((0, 'Draft'), (1, 'Published'))
 
 
 class Recipe(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="recipes_posted"
+        User, on_delete=models.CASCADE, related_name='recipes_posted'
     )
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
@@ -24,10 +24,25 @@ class Recipe(models.Model):
         User, related_name='recipes_liked', blank=True)
 
     class Meta:
-        ordering = ["-created_on"]
+        ordering = ['-created_on']
 
     def __str__(self):
         return self.title
 
     def number_of_likes(self):
         return self.likes.count()
+
+
+class Comment(models.Model):
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f'Comment by {self.user.username} on {self.recipe.title}'
