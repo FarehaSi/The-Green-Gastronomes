35b7950f01695dc57b857566533565cec907d624
FarehaSi <>
2023-07-14 18:12:59 +0000

    Update admin.py to allow admin to approve user comments

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -11,3 +11,15 @@ class RecipeAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
     summernote_fields = ('instructions')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'user', 'content', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+
