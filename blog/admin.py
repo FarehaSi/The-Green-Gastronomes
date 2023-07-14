6f454bd0ffb16066458357a979596cb5d7f23b0b
FarehaSi <>
2023-07-14 15:07:32 +0000

    Create a superuser account to administer the site and install summernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Recipe
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'status', 'created_on')
+    search_fields = ['title']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('instructions')
