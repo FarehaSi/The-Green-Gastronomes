9bbdaa3350616bfd97812046babe6c875cbeb096
FarehaSi <>
2023-07-14 18:40:42 +0000

    Update admin.py to allow admin to approve user recipes

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Recipe, Comment
+from .models import Recipe, Comment, UserProfile
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -23,3 +23,13 @@ class CommentAdmin(admin.ModelAdmin):
         queryset.update(approved=True)
 
 
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'email')
+    actions = ['approve_recipes']
+
+    def approve_recipes(self, request, queryset):
+        for user_profile in queryset:
+            recipes_to_approve = Recipe.objects.filter(author=user_profile.user, approved=False)
+            recipes_to_approve.update(approved=True)
+        self.message_user(request, f"{queryset.count()} user profiles with recipes approved.")
