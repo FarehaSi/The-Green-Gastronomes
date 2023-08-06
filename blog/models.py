7054b088ad1b9a3b9066800968fd4cc10a510a97
FarehaSi <>
2023-08-06 18:37:23 +0000

    Add comments icon to the recipe card on homepage

--- blog/models.py
+++ blog/models.py
@@ -42,6 +42,9 @@ class Recipe(models.Model):
     def number_of_likes(self):
         return self.likes.count()
 
+    def number_of_comments(self):
+        return self.comments.count()
+
 
 class Comment(models.Model):
     recipe = models.ForeignKey(
